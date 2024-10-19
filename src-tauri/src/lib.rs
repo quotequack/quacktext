64c265c4631b73bb7e836ac97b1177d8f3c1e6b6
QuoteQuack <118599418+quotequack@users.noreply.github.com>
2024-10-19 12:55:59 +0300

    Fixed bug

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -40,7 +40,7 @@ fn enigo(input: String) {
     println!("{}", &input);
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
     let _ = enigo.text(&input);
-    let _ = enigo.key(Key::Return);
+    let _ = enigo.key(Key::Return, Direction::Click);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
